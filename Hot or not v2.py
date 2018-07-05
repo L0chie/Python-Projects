293b8458a486c45d0e839f2dbab1363ce065ffe7
L0chie <40861217+L0chie@users.noreply.github.com>
2018-07-06 00:07:58 +1000

    Add files via upload

---
+++ Hot or not v2.py
@@ -0,0 +1,404 @@
+import time
+
+print("Lets play a game...")
+print(" ")
+time.sleep(0.5)
+
+print("Commands: 'Choices' or 'Characteristics' (Case sensitive)")
+time.sleep(0.5)
+
+def hot_or_not():
+
+# POOL
+
+    Ariana = {"age":"24", "height":"1.53", "rating":"9", "ass":"tight", "boobs":"small"}
+
+    Alissa = {"age":"21", "height":"1.78", "rating":"9", "ass":"tight", "boobs":"small"}
+
+    Jlaw = {"age":"27", "height":"1.75", "rating":"8", "ass":"tight", "boobs":"medium"}
+
+    Demi = {"age":"24", "height":"1.53", "rating":"7", "ass":"thicc", "boobs":"medium"}
+
+    Camila = {"age":"20", "height":"1.57", "rating":"8", "ass":"tight", "boobs":"small"}
+
+    Taylor = {"age":"27", "height":"1.8", "rating":"7", "ass":"tight", "boobs":"small"}
+
+    Katy = {"age":"33", "height":"1.75", "rating":"6", "ass":"flat", "boobs":"big"}
+
+    Selena = {"age":"25", "height":"1.65", "rating":"7", "ass":"tight", "boobs":"small"}
+
+    Gigi = {"age":"22", "height":"1.79", "rating":"8", "ass":"tight", "boobs":"small"}
+
+    Kate = {"age":"25", "height":"1.78", "rating":"8", "ass":"thicc", "boobs":"big"}
+
+    jessica = {"age":"36", "height":"1.69", "rating":"7", "ass":"tight", "boobs":"small"}
+
+    Gal = {"age":"32", "height":"1.78", "rating":"9", "ass":"flat", "boobs":"small"}
+
+    print(" ")
+    user_input = str(input('Input: '))
+
+# ARIANA GRANDE
+
+    if user_input == 'Ariana':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+
+    elif user_input == 'Ariana ass':
+        print(Ariana["ass"])
+        print(" ")
+
+    elif user_input == 'Ariana rating':
+        print(Ariana["rating"])
+        print(" ")
+
+    elif user_input == 'Ariana age':
+        print(Ariana["age"])
+        print(" ")
+
+    elif user_input == 'Ariana boobs':
+        print(Ariana["boobs"])
+        print(" ")
+
+    elif user_input == 'Ariana height':
+        print(Ariana["height"])
+        print(" ")
+
+# ALISSA VIOLET
+
+    elif user_input == 'Alissa':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Alissa ass':
+        print(Alissa["ass"])
+        print(" ")
+
+    elif user_input == 'Alissa rating':
+        print(Alissa["rating"])
+        print(" ")
+
+    elif user_input == 'Alissa age':
+        print(Alissa["age"])
+        print(" ")
+
+    elif user_input == 'Alissa height':
+        print(Alissa["height"])
+        print(" ")
+
+    elif user_input == 'Alissa boobs':
+        print(Alissa["boobs"])
+        print(" ")
+
+# jennifer Lawrence 
+
+    elif user_input == 'Jlaw':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Jlaw ass':
+        print(Alissa["ass"])
+        print(" ")
+
+    elif user_input == 'Alissa rating':
+        print(Jlaw["rating"])
+        print(" ")
+
+    elif user_input == 'Alissa age':
+        print(Jlaw["age"])
+        print(" ")
+
+    elif user_input == 'Jlaw height':
+        print(Jlaw["height"])
+        print(" ")
+
+    elif user_input == 'Jlaw boobs':
+        print(Jlaw["boobs"])
+        print(" ")
+
+# Demi Lovato 
+
+    elif user_input == 'Demi':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Demi ass':
+        print(Demi["ass"])
+        print(" ")
+
+    elif user_input == 'Demi rating':
+        print(Demi["rating"])
+        print(" ")
+
+    elif user_input == 'Demi age':
+        print(Demi["age"])
+        print(" ")
+
+    elif user_input == 'Demi height':
+        print(Demi["height"])
+        print(" ")
+
+    elif user_input == 'Demi boobs':
+        print(Demi["boobs"])
+        print(" ")
+
+# Camila Cabello 
+
+    elif user_input == 'Camila':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Camila ass':
+        print(Camila["ass"])
+        print(" ")
+
+    elif user_input == 'Camila rating':
+        print(Camila["rating"])
+        print(" ")
+
+    elif user_input == 'Camila age':
+        print(Camila["age"])
+        print(" ")
+
+    elif user_input == 'Camila height':
+        print(Camila["height"])
+        print(" ")
+
+    elif user_input == 'Camila boobs':
+        print(Camila["boobs"])
+        print(" ")
+
+# Taylor Swift
+
+    elif user_input == 'Taylor':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Taylor ass':
+        print(Taylor["ass"])
+        print(" ")
+
+    elif user_input == 'Taylor rating':
+        print(Taylor["rating"])
+        print(" ")
+
+    elif user_input == 'Taylor age':
+        print(Taylor["age"])
+        print(" ")
+
+    elif user_input == 'Taylor height':
+        print(Taylor["height"])
+        print(" ")
+
+    elif user_input == 'Taylor boobs':
+        print(Taylor["boobs"])
+        print(" ")
+
+# Katy Perry 
+
+    elif user_input == 'Katy':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Katy ass':
+        print(Katy["ass"])
+        print(" ")
+
+    elif user_input == 'Katy rating':
+        print(Katy["rating"])
+        print(" ")
+
+    elif user_input == 'Katy age':
+        print(Katy["age"])
+        print(" ")
+
+    elif user_input == 'Katy height':
+        print(Katy["height"])
+        print(" ")
+
+    elif user_input == 'Katy boobs':
+        print(Katy["boobs"])
+        print(" ")
+
+# Selena Gomez
+
+    elif user_input == 'Selena':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Selena ass':
+        print(Selena["ass"])
+        print(" ")
+
+    elif user_input == 'Selena rating':
+        print(Selena["rating"])
+        print(" ")
+
+    elif user_input == 'Selena age':
+        print(Selena["age"])
+        print(" ")
+
+    elif user_input == 'Selena height':
+        print(Selena["height"])
+        print(" ")
+
+    elif user_input == 'Selena boobs':
+        print(Selena["boobs"])
+        print(" ")
+
+# Gigi Hadid
+
+    elif user_input == 'Gigi':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Gigi ass':
+        print(Gigi["ass"])
+        print(" ")
+
+    elif user_input == 'Gigi rating':
+        print(Gigi["rating"])
+        print(" ")
+
+    elif user_input == 'Gigi age':
+        print(Gigi["age"])
+        print(" ")
+
+    elif user_input == 'Gigi height':
+        print(Gigi["height"])
+        print(" ")
+
+    elif user_input == 'Gigi boobs':
+        print(Gigi["boobs"])
+        print(" ")
+
+# Kate Upton 
+
+    elif user_input == 'Kate':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Kate ass':
+        print(Kate["ass"])
+        print(" ")
+
+    elif user_input == 'Kate rating':
+        print(Kate["rating"])
+        print(" ")
+
+    elif user_input == 'Kate age':
+        print(Kate["age"])
+        print(" ")
+
+    elif user_input == 'Kate height':
+        print(Kate["height"])
+        print(" ")
+
+    elif user_input == 'Kate boobs':
+        print(Kate["boobs"])
+        print(" ")
+
+# Jessica Alba
+
+    elif user_input == 'Jessica':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Jessica ass':
+        print(Jessica["ass"])
+        print(" ")
+
+    elif user_input == 'Jessica rating':
+        print(Jessica["rating"])
+        print(" ")
+
+    elif user_input == 'Jessica age':
+        print(jessica["age"])
+        print(" ")
+
+    elif user_input == 'Demi height':
+        print(Demi["height"])
+        print(" ")
+
+    elif user_input == 'Jessica boobs':
+        print(Jessica["boobs"])
+        print(" ")
+
+# Demi Lovato 
+
+    elif user_input == 'Gal':
+            for i in range(10):
+                print("hot".upper())
+                print(" ")
+                
+    elif user_input == 'Gal ass':
+        print(Gal["ass"])
+        print(" ")
+
+    elif user_input == 'Gal rating':
+        print(Gal["rating"])
+        print(" ")
+
+    elif user_input == 'Gal age':
+        print(Gal["age"])
+        print(" ")
+
+    elif user_input == 'Gal height':
+        print(Gal["height"])
+        print(" ")
+
+    elif user_input == 'Gal boobs':
+        print(Gal["boobs"])
+        print(" ")
+        
+# Other Commands
+    elif user_input == 'Choices':
+        print("""
+Ariana Grande (Ariana)
+Alissa Violet (Alissa)
+Jennifer lawrence (Jlaw)
+Demi Lovato (Demi)
+Camila Cabello (Camila)
+Taylor Swift (Taylor)
+Katy Perry (Katy)
+Selena Gomez (Selena)
+Gigi Hadid (Gigi)
+Kate Upton (Kate)
+Jessica Alba (Jessica)
+Gal Gadot (Gal) """)
+
+    elif user_input == 'Characteristics':
+        print("""
+age
+height
+rating
+ass
+boobs""")
+              
+    else:
+        print("She's not worthy of being here")
+        print(" ")
+
+
+i=0
+print(""""""
+==========================================================
+|   Hot or Not                                           |
+|   Choose Someone (｡◕‿‿◕｡)                              |
+=========================================================
+"""""")
+while i<1000:
+    hot_or_not()
+    i+=1
+
