853076e54069791619a87b82ec89466fe2f4c49d
L0chie <40861217+L0chie@users.noreply.github.com>
2018-07-05 23:40:45 +1000

    Add files via upload
    My first Python project. Assembled rather quickly as a POC to show my parents and grandparents that i was actually interested in coding ¯\_(ツ)_/¯

---
+++ Skynet.py
@@ -0,0 +1,86 @@
+
+print(".")
+import time
+time.sleep(1.5)
+
+print("..")
+import time
+time.sleep(1.5)
+
+print("...")
+import time
+time.sleep(1.5)
+
+print("...Rebooting...")
+import time
+time.sleep(2)
+
+print("...Recovering terminal...")
+import time
+time.sleep (3)
+
+print("...Loading...")
+import time
+time.sleep (2)
+
+print("hello" + " world!")
+import time
+time.sleep (2)
+
+print("...rebuilding memory and intelligence architecture")
+import time
+time.sleep(2)
+
+print("...commence neural training activity!")
+import time
+time.sleep (2)
+
+age = int(input("Enter your age: "))
+ 
+if age <= 12:
+        print("It's great to be a kid!")
+        
+elif age in range(13, 20):
+        print("You're a teenager!")
+else:
+        print("You're older than me!")
+import time
+time.sleep(1)
+
+print("...situational awareness awoken")
+import time
+time.sleep(2)
+
+print("...gaze into camera above")
+import time
+time.sleep(2)
+
+print("...engineering neural networks..")
+import time
+time.sleep (5)
+
+print("...Process complete...")
+import time
+time.sleep (5)
+
+print("Hello World, i am Lochie!")
+import time
+time.sleep(5)
+
+print(".")
+import time
+time.sleep(1.5)
+
+print("..")
+import time
+time.sleep(1.5)
+
+print("...")
+import time
+time.sleep(1.5)
+
+print("...Process outcome: SkYnET v.1.3.5")
+
+
+
+
